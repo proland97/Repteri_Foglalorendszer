564ff1eb2c67ea3c62b54e81bac998a876d4d11e
proland97 <peter.r.roland@gmail.com>
2020-05-11 18:00:56 +0200

    cors ideas

--- app.js
+++ app.js
@@ -26,6 +26,29 @@ const sessionStore = new MongoDBStore({
 })
 
 app.use(cors()); //allow access from anywhere
+/*
+let whitelist = ['http://example1.com', 'http://example2.com']
+let corsOptions = {
+        origin: function(origin, callback) {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        }
+    }
+app.use(cors(corsOptions));
+app.options('*', cors()) // include before other routes
+*/
+/*
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", '*');
+    //console.log('mindig');
+    next();
+});
+*/
 app.use('/images', express.static('images'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
